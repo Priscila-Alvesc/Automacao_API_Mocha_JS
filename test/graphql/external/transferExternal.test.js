140e25e5e9ea88948700cf75974036456d6470f8
alvescostapriscila@gmail.com <alvescostapriscila@gmail.com>
2025-09-06 11:16:08 -0300

    organizando

---
+++ test/graphql/external/transferExternal.test.js
@@ -0,0 +1,78 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Testes de Transferência API GraphQL', () => {
+    beforeEach(async() =>{
+        const loginResponse = await request('http://localhost:4000/graphql')
+            .post('')
+            .send({
+                query: `
+                    mutation LoginUser($username: String!, $password: String!) { 
+                        loginUser(username: $username, password: $password) { 
+                            token 
+                        }
+                    }`,
+                variables: {
+                    username: 'julio',
+                    password: '123456'
+                }
+            });
+            
+        token = loginResponse.body.data.loginUser.token;
+    });
+
+    it('Validar que é possível transferir grana entre duas contas', async () => {
+     
+        const response = await request('http://localhost:4000/graphql')
+            .post('')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                query: `
+                    mutation CreateTransfer($from: String!, $to: String!, $value: Float!) {
+                        createTransfer(from: $from, to: $to, value: $value) {
+                            date
+                            from
+                            to
+                            value
+                        }
+                    }
+                `,
+                variables: {
+                    from: 'julio',
+                    to: 'priscila',
+                    value: 15
+                }
+            });
+
+        expect(response.status).to.equal(200);
+        expect(response.body.data.createTransfer).to.have.property('from', 'julio');
+        expect(response.body.data.createTransfer).to.have.property('to', 'priscila');
+        expect(response.body.data.createTransfer).to.have.property('value', 15);
+    });
+
+    it('Validar que não é possível transferir de uma conta sem saldo suficiente', async () => {
+               
+        const response = await request('http://localhost:4000/graphql')
+            .post('')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                query: `
+                    mutation CreateTransfer($from: String!, $to: String!, $value: Float!) {
+                        createTransfer(from: $from, to: $to, value: $value) {
+                            date
+                            from
+                            to
+                            value
+                        }
+                    }
+                `,
+                variables: {
+                    from: 'julio',
+                    to: 'priscila',
+                    value: 10000.01
+                }
+            });
+
+        expect(response.body.errors[0].message).to.equal('Saldo insuficiente');
+    });
+});
